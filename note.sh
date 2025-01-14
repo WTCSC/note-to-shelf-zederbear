24960eec4ca3a6b3e123b2bdb61700709d7ccb35
zederbear <hooverzed@gmail.com>
2025-01-14 15:18:50 -0700

    added comments

--- note.sh
+++ note.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 NOTE_FILE="notes.txt"
 
+# Add note
 if [ $1 == "add" ]; then
     echo $(date +"%F %T") - $2 >> $NOTE_FILE
     echo Note added successfully
+# List notes
 elif [ $1 == "list" ]; then
     cat $NOTE_FILE
+# Search notes
 elif [ $1 == "search" ]; then
     grep -i "$2" $NOTE_FILE
 else
