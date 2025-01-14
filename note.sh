92a300744f79ca1be8ddf40a2f818e7cebaafd6c
zederbear <hooverzed@gmail.com>
2025-01-14 15:16:42 -0700

    did readme and fixed grep command to have it in qoutes and un-hardcoded the file name

--- note.sh
+++ note.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+NOTE_FILE="notes.txt"
 
 if [ $1 == "add" ]; then
-    echo $(date +"%F %T") - $2 >> notes.txt
+    echo $(date +"%F %T") - $2 >> $NOTE_FILE
     echo Note added successfully
 elif [ $1 == "list" ]; then
-    cat notes.txt
+    cat $NOTE_FILE
 elif [ $1 == "search" ]; then
-    grep -i $2 notes.txt
+    grep -i "$2" $NOTE_FILE
 else
     exit 1
 fi
