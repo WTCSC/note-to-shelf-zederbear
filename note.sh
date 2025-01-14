6f12aebf1fdb38023bb38969390587ebd118fa3f
zederbear <hooverzed@gmail.com>
2025-01-14 14:59:09 -0700

    Coded the note.sh script

--- note.sh
+++ note.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+if [ $1 == "add" ]; then
+    echo $(date +"%F %T") - $2 >> notes.txt
+    echo Note added successfully
+elif [ $1 == "list" ]; then
+    cat notes.txt
+elif [ $1 == "search" ]; then
+    grep -i $2 notes.txt
+else
+    exit 1
+fi
+
+exit 0
